35aac4aff6637692b347a23848d5d4fb986ec662
Prakhar Saraswat <prakhar.saraswat@geminisolutions.com>
2023-08-03 22:37:41 +0530

    Classes in ES6

--- index.js
+++ index.js
@@ -8,4 +8,30 @@ const add = (a, b) => a + b;
 
 console.log(add(5, 10)); // Output: 15
 
-
+// Classes in ES6
+class Animal {
+    constructor(name, species) {
+      this.name = name;
+      this.species = species;
+    }
+  
+    makeSound() {
+      return "Some generic sound";
+    }
+  }
+  
+  class Dog extends Animal {
+    constructor(name, breed) {
+      super(name, "Dog");
+      this.breed = breed;
+    }
+  
+    makeSound() {
+      return "Woof!";
+    }
+  }
+  
+  const myDog = new Dog("Buddy", "Labrador");
+  console.log(myDog.name); // Output: "Buddy"
+  console.log(myDog.species); // Output: "Dog"
+  console.log(myDog.makeSound()); // Output: "Woof!"
